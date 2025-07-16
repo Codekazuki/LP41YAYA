b15f88ab17fd3032ec7fb86f4bfcb48c438ad121
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-07-16 21:44:33 +0100

    lp41 leadership summit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
-import "./styles/index.css";
 
 function App() {
   return (

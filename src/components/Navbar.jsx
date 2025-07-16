c1ee40e46fa1e1b862f1eb9f20fffa95c60fda0b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-07-16 14:05:49 +0100

    lp41 leadership summit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
+import logo1 from "../assets/rccg-logo.png";
+import logo2 from "../assets/rccg-logo-2.png";
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <div className='navbar-container'>
         <div className='navbar-logo'>
-          <img src='/logo1.png' alt='Logo 1' />
-          <img src='/logo2.png' alt='Logo 2' />
+          <img src={logo1} alt='RCCG Logo' />
+          <img src={logo2} alt='RCCG Logo 2' />
         </div>
         <div className='navbar-links'>
           <Link to='/'>Home</Link>
-          <Link to='/about'>About</Link>
-          <Link to='/contact'>Contact</Link>
+          <Link to='/'>About</Link>
+          <Link to='/'>Contact</Link>
         </div>
       </div>
     </nav>

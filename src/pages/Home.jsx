42a9a542411e26865a4fd298d2221c0194305d93
Ayodele Agbowuro <agbowurotoyin8@gmail.com>
2025-07-17 16:38:59 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <Navbar />
       <main className='main-content'>
         <h1>RCCG LP41 YAYA LEADERSHIP SUMMIT 2025</h1>
-        <h1>EFFECTIVE LEADERSHIP & STEWARDSHIP</h1>
+        <h1>EFFECTIVE LEADERSHIP AND STEWARDSHIP</h1>
         <h2>Meet our speakers</h2>
         <CardList />
       </main>

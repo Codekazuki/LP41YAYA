e0ca155f314b7dbf201c271714cbc240474ceb3c
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-07-17 05:13:46 +0100

    lp41 leadership summit

--- src/teamData.js
+++ src/teamData.js
@@ -6,14 +6,6 @@ import wumi from "./assets/wumi.jpeg";
 const teamData = [
   {
     id: 1,
-    image: busuyi,
-    name: "Pastor Busuyi Lawal",
-    title: "RCCG RYP REGION 26",
-    description:
-      "Pastor Busuyi Lawal is a dynamic spiritual leader and the Regional Youth Pastor for RCCG Region 26, where he also pastors Gate of Heaven Parish, a parish directly linked to the  regional headquarters.<br> </br> A passionate advocate for youth development, he is dedicated to raising spiritually grounded, purpose-driven young leaders. <br> </br> He holds a BSc in Chemical Engineering and an MBA from the University of Lagos, and is a member of the Chartered Institute of Bankers of Nigeria (CIBN). <br> </br> Professionally, he serves as a Business Development Manager in a top Nigerian financial institution.With a rich ministry journey—from Youth Coordinator to Regional Youth Pastor—Pastor Lawal has led numerous impactful youth initiatives such as Youth Open Up (YOU) and Script From The Scripture Drama Ministry. <br> </br> His leadership has strengthened unity and spiritual depth among youth across all provinces in Region 26.He is happily married and blessed with biological and spiritual children. Pastor Lawal remains committed to raising a generation of leaders built on integrity, vision, and the Word of God.",
-  },
-  {
-    id: 2,
     image: ekpeyong,
     name: "Pastor Duke Ekpeyong",
     title: "TRINITY HOUSE",
@@ -21,12 +13,20 @@ const teamData = [
       "Pastor Ekpenyong Duke is a seasoned leader with a remarkable track record of excellence across ministry, corporate leadership, and education. <br> </br> With over three decades of pastoral experience in the Redeemed Christian Church of God (RCCG)—where he pioneered and pastored several parishes from 1995 to 2015—Pastor Duke is a trusted teacher of the Word, counselor, and administrator. <br> </br>  He has also served as a lecturer for over 10 years at the School of Disciples and the Redeemers Bible College, where he helped shape spiritual leaders and disciples across Nigeria.A firm believer in the synergy of faith and leadership, Pastor Duke holds an Executive MBA from the Lagos Business School. <br> </br> His distinguished professional journey includes serving as:CEO of CG EKO LLP—a groundbreaking Public-Private Partnership between the Lagos State Government and Coscharis Group.Group Managing Director of the University of Lagos Holding Company Ltd, overseeing diverse commercial ventures. <br> </br> Currently, he is the Director of General Services at Carloha Nigeria Ltd, a growing force in the auto assembly industry. <br> </br> Today, Pastor Duke continues to serve in ministry as a pastor at Trinity House Church, bringing faith-based insight into leadership, purpose, and nation-building. He is happily married to Ibok Duke, and they are blessed with three children.",
   },
   {
-    id: 3,
+    id: 2,
     image: fela,
     name: "Pastor Fela Bank Olemoh",
     title: "PASTOR FOUNDATION TRUTH ASSEMBLY",
     description:
       "Fela Bank-Olemoh is a dynamic and accomplished leader with a unique blend of experience spanning Ministry, Government, Business, and Family. <br> </br> His diverse expertise has enabled him to drive transformative change across sectors. Entrepreneurship: Co-founder of innovative ventures like Media Vision Ltd, Traction Learning Solutions, and Knit Technologies Ltd. <br> </br> He currently serves as Managing Director of Whatadeal Ltd—a global platform revolutionizing Travel, Events, and E-commerce following the 2024 merger  of Knit Tech Ltd and Deelaa Ltd.<br> </br> <b>Public Sector:</b> Fela has served in both state and federal government roles, including Special Adviser on Education to the Lagos State Governor (2015–2019) and SSA to the President on Education Interventions (2019–2023). He led impactful programs such as Project T-Max, Ready Set Work (RSW), and Code Lagos. <br> </br> <b>Ministry:</b> With over 25 years in ministry, he pioneered and pastored the Foundation of Truth Assembly, Ajah, for nearly a decade. He continues to inspire believers through teaching, mentorship, and a passion for Kingdom advancement",
+  },
+  {
+    id: 3,
+    image: busuyi,
+    name: "Pastor Busuyi Lawal",
+    title: "RCCG RYP REGION 26",
+    description:
+      "Pastor Busuyi Lawal is a dynamic spiritual leader and the Regional Youth Pastor for RCCG Region 26, where he also pastors Gate of Heaven Parish, a parish directly linked to the  regional headquarters.<br> </br> A passionate advocate for youth development, he is dedicated to raising spiritually grounded, purpose-driven young leaders. <br> </br> He holds a BSc in Chemical Engineering and an MBA from the University of Lagos, and is a member of the Chartered Institute of Bankers of Nigeria (CIBN). <br> </br> Professionally, he serves as a Business Development Manager in a top Nigerian financial institution.With a rich ministry journey—from Youth Coordinator to Regional Youth Pastor—Pastor Lawal has led numerous impactful youth initiatives such as Youth Open Up (YOU) and Script From The Scripture Drama Ministry. <br> </br> His leadership has strengthened unity and spiritual depth among youth across all provinces in Region 26.He is happily married and blessed with biological and spiritual children. Pastor Lawal remains committed to raising a generation of leaders built on integrity, vision, and the Word of God.",
   },
   {
     id: 4,
